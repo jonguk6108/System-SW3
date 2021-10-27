18abc2d8b11481db41bc963987fe0eb26bbf7efa
PARK JONGUK <jonguk6108@gmail.com>
2021-10-27 18:27:26 +0900

    lab9-1 commit

--- week9/lab9/text.cc
+++ week9/lab9/text.cc
@@ -16,7 +16,7 @@ class StringVar{
         void input_line(istream& ins);
         friend ostream& operator <<(ostream& outs, const StringVar& the_string);
         void operator =(const StringVar& right_side);
-        char *get_value();
+        char *get_value() const;
     private:
         char *value;
         int max_length;
@@ -28,6 +28,7 @@ class Text{
         Text(int size);
         Text();
         Text(const char a[]);
+        StringVar get_iword (int i) const;
         Text(const Text& text_obj);
         ~Text();
         int length() const;
@@ -38,37 +39,6 @@ class Text{
         StringVar* word;
         int max_length;
 };
-
-Text::Text(int size){
-    //construct with size
-}
-Text::Text(){
-    //default constructor
-}
-Text::Text(const char a[]){
-    //construct with array
-}
-Text::Text(const Text& text_obj){
-    //copy constructor
-}
-Text::~Text(){
-    //destructor
-}
-
-int Text::length() const{
-    //return total stored word num
-}
-
-void Text::input_line(istream& ins){
-    //get input from command line like StringVar getline
-}
-ostream& operator <<(ostream& outs, const Text& text){
-    //operator overloading
-}
-void Text::operator =(const Text& right_side){
-    //assignment operator overloading
-}
-
 StringVar::StringVar(int size) : max_length(size){
     value = new char[max_length + 1];
     value[0] = '\0';
@@ -98,7 +68,7 @@ ostream& operator <<(ostream& outs, const StringVar& the_string){
     outs << the_string.value;
     return outs;
 }
-char* StringVar::get_value(){
+char* StringVar::get_value() const{
     return value;
 }
 void StringVar::operator =(const StringVar& right_side){
@@ -112,6 +82,111 @@ void StringVar::operator =(const StringVar& right_side){
         value[i] = right_side.value[i];
     value[new_length] = '\0';
 }
+
+Text::Text(int size){
+    //construct with size
+    max_length = size;
+    word = new StringVar[max_length + 1];
+    word[0] = StringVar(" ");
+}
+Text::Text(){
+    //default constructor
+    max_length = 50;
+    word = new StringVar[max_length + 1];
+    word[0] = StringVar(" ");
+}
+Text::Text(const char a[]){
+    //construct with array
+    int length = 0;
+    for(int i = 0; i < strlen(a); i++) {
+        if(i == 0)
+            continue;
+        if(a[i] == ' ' && a[i-1] != ' ')
+            length++;
+        if(i == strlen(a) - 1)
+            length++;
+    }
+
+    max_length = length;
+    word = new StringVar[max_length + 1];
+
+    char ctmp[30];
+    int s = 0;
+    int f = 0;
+    for(int i = 0; i < max_length; i++) {
+        while(1) {
+            if(a[f] == '\0') break;
+            if(a[f] == ' ') break;
+            f++;
+        }
+        for(int j = s; j < f; j++)
+            ctmp[j-s] = a[j];
+        ctmp[f-s] = '\0';
+        word[i] = StringVar(ctmp);
+        s = f+1;
+        f = s;
+    }
+    word[max_length] = StringVar(" ");
+}
+StringVar Text::get_iword (int i) const{
+    return word[i];
+}
+Text::Text(const Text& text_obj){
+    //copy constructor
+    max_length = text_obj.length();
+    word = new StringVar[max_length + 1];
+    for(int i = 0; i < max_length + 1; i++)
+        word[i] = text_obj.get_iword(i); 
+}
+Text::~Text(){
+    //destructor
+    delete [] word;
+}
+
+int Text::length() const{
+    //return total stored word num
+    for(int i = 0; i < max_length; i++)
+        if(strcmp( word[i].get_value(), " ") == 0)
+            return i;
+    return max_length;
+}
+
+void Text::input_line(istream& ins){
+    //get input from command line like StringVar getline
+    StringVar stmp;
+    stmp.input_line(ins);
+
+    Text ttmp(stmp.get_value());
+    
+    for(int i = 0; i < max_length; i++)
+        word[i] = ttmp.get_iword(i).get_value();
+    word[max_length] = StringVar(" ");
+
+}
+ostream& operator <<(ostream& outs, const Text& text){
+    //operator overloading
+    int length = text.length();
+    for(int i = 0; i <= length; i++) {
+        if( strcmp(text.get_iword(i).get_value(), " ") == 0)
+            return outs;
+        outs << text.get_iword(i).get_value() << " "; 
+    }
+}
+void Text::operator =(const Text& right_side){
+    //assignment operator overloading
+    int new_length = right_side.length();
+    if( new_length > max_length ) {
+        delete[] word;
+        max_length = new_length;
+        word = new StringVar[max_length + 1];
+        for(int i = 0; i < max_length + 1; i++)
+            word[i] = right_side.get_iword(i); 
+    }
+    for( int i = 0; i < new_length; i++ )
+        word[i] = right_side.get_iword(i); 
+    word[new_length] = StringVar(" ");
+}
+
 void conversation(int max_name_size);
 
 int main()
@@ -129,13 +204,13 @@ void conversation(int max_name_size)
     using namespace std;
     //test deafult constructor
     Text text_default(max_name_size);
-    cout << text_default << text.length() <<endl;
+    cout << text_default << text_default.length() <<endl;
     //test array constructor
     Text text(TEXT);
     cout << text <<  text.length()<<endl;
     //test copy constructor
     Text text1(text);
-    cout << text1 <<  text.length()<<endl;
+    cout << text1 <<  text1.length()<<endl;
     //test assignment operator overloading
     Text text2(2);
     text2 = text;
